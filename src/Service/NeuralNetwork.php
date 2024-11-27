053882a4a9f3084fefeb1e79df90043b83fa5363
Jurij Schlaht <jschlaht@mac.com>
2024-11-27 11:25:12 +0100

    add initial parameters add getters for initial parameters add private function for calculation of weight matrices

--- src/Service/NeuralNetwork.php
+++ src/Service/NeuralNetwork.php
@@ -4,9 +4,48 @@ namespace App\Service;
 
 class NeuralNetwork
 {
-    public function init()
-    {
+    private array $weightsInputToHidden;
+    private array $weightsHiddenToOutput;
 
+    public function __construct(
+        private int $inputNodes,
+        private int $hiddenNodes,
+        private int $outputNodes,
+        private float $learningRate
+    )
+    {
+        $this->weightsInputToHidden = $this->createWeightsMatrix($this->hiddenNodes, $this->inputNodes);
+        $this->weightsHiddenToOutput = $this->createWeightsMatrix($this->outputNodes, $this->hiddenNodes);
+    }
+
+    public function getInputNodes(): int
+    {
+        return $this->inputNodes;
+    }
+
+    public function getHiddenNodes(): int
+    {
+        return $this->hiddenNodes;
+    }
+
+    public function getOutputNodes(): int
+    {
+        return $this->outputNodes;
+    }
+
+    public function getLearningRate(): float
+    {
+        return $this->learningRate;
+    }
+
+    public function getWeightsInputToHidden(): array
+    {
+        return $this->weightsInputToHidden;
+    }
+
+    public function getWeightsHiddenToOutput(): array
+    {
+        return $this->weightsHiddenToOutput;
     }
 
     public function train()
@@ -17,5 +56,17 @@ class NeuralNetwork
     public function query()
     {
 
+    }
+
+    private function createWeightsMatrix(int $rows, int $cols)
+    {
+        $matrix = [];
+
+        for ($i = 0; $i < $rows; $i++) {
+            for ($j = 0; $j < $cols; $j++) {
+                $matrix[$i][$j] = mt_rand(-1000, 1000)/1000;
+            }
+        }
+        return $matrix;
     }
 }
--- tests/Service/NeuralNetworkTest.php
+++ tests/Service/NeuralNetworkTest.php
@@ -8,9 +8,36 @@ use PHPUnit\Framework\TestCase;
 class NeuralNetworkTest extends TestCase
 {
     public function testNeuralNetwork() {
-        $neuralNetwork = new NeuralNetwork();
+        $neuralNetwork = new NeuralNetwork(3,3,3,0.3);
         $this->assertInstanceOf(NeuralNetwork::class, $neuralNetwork);
+        $this->assertSame($neuralNetwork->getInputNodes(), 3);
+        $this->assertSame($neuralNetwork->getHiddenNodes(), 3);
+        $this->assertSame($neuralNetwork->getOutputNodes(), 3);
+        $this->assertSame($neuralNetwork->getLearningRate(), 0.3);
     }
 
+    public function testWeightsMatrixValues() {
+        $numberOfInputNodes = 3;
+        $numberOfHiddenNodes = 5;
+        $numberOfOutputNodes = 2;
+        $learningRate = 0.3;
+        $neuralNetwork = new NeuralNetwork($numberOfInputNodes, $numberOfHiddenNodes, $numberOfOutputNodes, $learningRate);
+        $weightsInputToHidden = $neuralNetwork->getWeightsInputToHidden();
+        $weightsHiddenToOutout = $neuralNetwork->getWeightsHiddenToOutput();
+
+        $this->assertEquals(count($weightsInputToHidden), $numberOfHiddenNodes);
+        $this->assertEquals(count($weightsHiddenToOutout), $numberOfOutputNodes);
+
+        $this->assertEquals(count($weightsInputToHidden[0]), $numberOfInputNodes);
+        $this->assertEquals(count($weightsHiddenToOutout[0]), $numberOfHiddenNodes);
+
+        $min = min(array_map('min', $weightsInputToHidden));
+        $max = max(array_map('max', $weightsInputToHidden));
+
+        $this->assertTrue(min(array_map('min', $weightsInputToHidden)) > -1);
+        $this->assertTrue(max(array_map('max', $weightsInputToHidden)) < 1);
+        $this->assertTrue(min(array_map('min', $weightsHiddenToOutout)) > -1);
+        $this->assertTrue(max(array_map('max', $weightsHiddenToOutout)) < 1);
+    }
 
 }
